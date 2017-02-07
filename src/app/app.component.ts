eefefd2f3f10833e582e7d0e8717566d0c128e22
RavikiranIndore <ravikiran.indore@tejgyan.org>
2017-02-08 05:25:28 +0530

    Update Comments in app.component file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ export class MyApp {
 
   }
 
-  // This method initializes the App
+  // This method initializes the Application
   initializeApp() {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
